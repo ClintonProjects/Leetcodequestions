c0ef7d99071b5af59c8f8b124b558276915f37e3
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-20 06:08:07 +0100

    Time: 8 ms (91.42%) | Memory: 60.5 MB (21.31%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,12 +1,12 @@
 class Solution {
 
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-            if (map.get(i) > 1) return true;
-         }
+        // Not really in the middle.
+        // Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // for (int i : nums) {
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
+        //     if (map.get(i) > 1) return true;
+        //  }
 
 
         //good memory slow run time
@@ -20,10 +20,10 @@ class Solution {
         // return false;
 
         //bad memory good run time
-        // Set<Integer> hs = new HashSet<>();
+        Set<Integer> hs = new HashSet<>();
 
-        // for (int i : nums) 
-        // if (!hs.add(i)) return true;
+        for (int i : nums) 
+        if (!hs.add(i)) return true;
         
         return false;
     }
