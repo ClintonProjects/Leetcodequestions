21aaa689e4ffdf449cb63298f94dc4fe286b5f22
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-20 06:14:31 +0100

    Time: 5 ms (54.01%) | Memory: 42.7 MB (88.18%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) 
+        return false;
+        
+        char[] word1 = new char[255];
+        char[] word2 = new char[255];
+
+        for (int i = 0; i < s.length(); i++) {
+            word1[s.charAt(i) - 'a']++;
+            word2[t.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 255; i++) {
+            if (word1[i] != word2[i])
+            return false;
+        }
+        
+
+        return true;
+        
+
+    }
+}
