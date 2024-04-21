445a7672585178c95a998a145043cf1e237cd786
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-22 00:50:53 +0100

    Time: 7 ms (20.57%) | Memory: 42.7 MB (13.49%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,16 +1,14 @@
 class Solution {
+
     public String mergeAlternately(String word1, String word2) {
         //boolean switch = false;
         int l = 0, r = 0;
         String result = "";
         while (l < word1.length() || r < word2.length()) {
-            // System.out.println(result);
-            if ((r == l && l < word1.length())  || r == word2.length() &&  l != word1.length() ) {
+            if ((r == l && l < word1.length()) || r == word2.length() && l != word1.length()) {
                 result += word1.charAt(l);
                 l++;
-            }
-            
-            if ((r < l && r < word2.length()) || l == word1.length() &&  r != word2.length()) {
+            } else if ((r < l && r < word2.length()) || l == word1.length() && r != word2.length()) {
                 result += word2.charAt(r);
                 r++;
             }
