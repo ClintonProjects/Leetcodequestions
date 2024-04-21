034f42dd406d98dc85ceed708b4207198a9e8245
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-22 00:53:03 +0100

    Time: 1 ms (85.29%) | Memory: 41.3 MB (88.34%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,19 +1,19 @@
 class Solution {
 
     public String mergeAlternately(String word1, String word2) {
-        //boolean switch = false;
         int l = 0, r = 0;
-        String result = "";
+        StringBuilder sb = new StringBuilder();
         while (l < word1.length() || r < word2.length()) {
+           
             if ((r == l && l < word1.length()) || r == word2.length() && l != word1.length()) {
-                result += word1.charAt(l);
+                sb.append(word1.charAt(l));
                 l++;
             } else if ((r < l && r < word2.length()) || l == word1.length() && r != word2.length()) {
-                result += word2.charAt(r);
+                sb.append(word2.charAt(r));
                 r++;
             }
         }
 
-        return result;
+        return sb.toString();
     }
 }
