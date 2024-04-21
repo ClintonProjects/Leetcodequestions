2678952769fcd7a0ab3bfe5df40b8a70435f43f0
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 18:43:57 +0100

    Time: 8 ms (6.84%) | Memory: 42.9 MB (88.70%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] r = new int[nums1.length];
+        int counter = 0;
+        int counter2 = 0;
+        int lastCount = 0;
+
+        for (int i = 0; i < nums1.length; i++)  r[i] = -1;
+        
+
+        for (int i = 0; i < nums1.length; i++) {
+            boolean check = false;
+            for (int j = 0; j < nums2.length; j++) {
+                if (!check && nums1[i] == nums2[j]) check = true;
+
+                if (nums1[i] < nums2[j] && check) {
+                    //System.out.println(nums1[i] + " " +  nums2[j]);
+                    counter = j;
+                    r[i] = nums2[j];
+                    counter++;
+                    break;
+                }
+            }
+            check = false;
+        }
+
+        
+
+
+        
+        return r;
+    }
+}
