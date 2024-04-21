6ca076965a82049b47714571481b441faadc0e42
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 19:06:45 +0100

    Time: 6 ms (13.82%) | Memory: 42.7 MB (98.04%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -5,7 +5,7 @@ class Solution {
         int counter = 0;
         boolean check = false;
         Arrays.fill(r, -1);
-    
+     
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 if (!check && nums1[i] == nums2[j]) check = true;
@@ -17,11 +17,6 @@ class Solution {
             }
             check = false;
         }
-
-        
-
-
-        
         return r;
     }
 }
