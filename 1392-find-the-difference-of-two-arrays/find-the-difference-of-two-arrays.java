3566582fc425354566ce4e1b3b966bca0a91edea
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 22:23:36 +0100

    Time: 9 ms (94.84%) | Memory: 45.4 MB (31.97%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -4,36 +4,70 @@ public class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         // Arrays.sort(nums1);
         // Arrays.sort(nums2);
-        int N = Math.max(nums1.length, nums2.length);
+
         HashSet<Integer> hs = new HashSet<Integer>();
         HashSet<Integer> hs2 = new HashSet<Integer>();
-        List<List<Integer>> n = new ArrayList<List<Integer>>();
-        
-        // Initialize lists inside n
-        n.add(new ArrayList<>());
-        n.add(new ArrayList<>());
+
+        List<Integer> l1 = new ArrayList<Integer>();
+        List<Integer> l2 = new ArrayList<Integer>();
+
+        int N = Math.max(nums1.length, nums2.length);
 
         for (int i = 0; i < N; i++) {
-            if (i < nums1.length) {
+            if (i < nums1.length) 
                 hs.add(nums1[i]);
-            }
-            if (i < nums2.length) {
+            
+            if (i < nums2.length) 
                 hs2.add(nums2[i]);
-            }
-        } 
+        }
+
+        for (int i : hs) {
+            if (!hs2.contains(i))
+                l1.add(i);
+        }
+
+        for (int i : hs2) {
+            if (!hs.contains(i))
+                l2.add(i);
+        }
+
+        List<List<Integer>> n = new ArrayList<List<Integer>>();
+        n.add(l1);
+        n.add(l2);
+
+        // int N = Math.max(nums1.length, nums2.length);
+        // HashSet<Integer> hs = new HashSet<Integer>();
+        // HashSet<Integer> hs2 = new HashSet<Integer>();
+        // List<List<Integer>> n = new ArrayList<List<Integer>>();
+        
+        // // Initialize lists inside n
+        // n.add(new ArrayList<>());
+        // n.add(new ArrayList<>());
+
+        // List<Integer> l1 = new new ArrayList<Integer>();
+
+        // for (int i = 0; i < N; i++) {
+        //     if (i < nums1.length) {
+        //         hs.add(nums1[i]);
+        //     }
+
+        //     if (i < nums2.length) {
+        //         hs2.add(nums2[i]);
+        //     }
+        // } 
         
 
-        for (int i : nums1) {
-            if (!hs2.contains(i) && !n.get(0).contains(i)) {
-                n.get(0).add(i);
-            }
-        }
+        // for (int i : nums1) {
+        //     if (!hs2.contains(i) && !l1.contains(i) {
+        //         n.get(0).add(i);
+        //     }
+        // }
 
-        for (int i : nums2) {
-            if (!hs.contains(i) && !n.get(1).contains(i)) {
-                n.get(1).add(i);
-            }
-        }
+        // for (int i : nums2) {
+        //     if (!hs.contains(i) && !n.get(1).contains(i)) {
+        //         n.get(1).add(i);
+        //     }
+        // }
 
         return n;
     }
