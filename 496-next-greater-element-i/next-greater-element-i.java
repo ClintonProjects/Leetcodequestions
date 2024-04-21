66b5d9f2e8dd38ad957fc644af51441c25bbb1c1
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 18:48:34 +0100

    Time: 7 ms (8.71%) | Memory: 43.1 MB (79.54%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -4,12 +4,11 @@ class Solution {
         int[] r = new int[nums1.length];
         int counter = 0;
         boolean check = false;
+        Arrays.fill(r, -1);
     
         for (int i = 0; i < nums1.length; i++) {
-            r[i] = -1;
             for (int j = 0; j < nums2.length; j++) {
                 if (!check && nums1[i] == nums2[j]) check = true;
-
                 if (nums1[i] < nums2[j] && check) {
                     r[i] = nums2[j];
                     counter = ++j;
