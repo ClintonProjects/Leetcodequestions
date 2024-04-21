01c1ca8018c4d6e6592f8a06a02e3d581f2f9bb4
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 04:36:05 +0100

    Time: 1 ms (73.21%) | Memory: 41.4 MB (26.45%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -4,9 +4,7 @@ class Solution {
         String str[] = s.split(" ");
 
         if (str.length != pattern.length()) return false;
-
-        // int[] map1 = new int[256];
-        // int[] map2 = new int[256];
+        
         Map<String, Character> map1 = new HashMap<>();
         Map<Character, String> map2 = new HashMap<>();
 
@@ -20,15 +18,6 @@ class Solution {
 
             map1.put(st, c);
             map2.put(c, st);
-
-            // char c = pattern.charAt(i);
-            // char e = str[i].charAt(0);
-
-            // if (map1[c] != 0 && map1[c] != e) return false;
-            // if (map2[e] != 0 && map2[e] !=  c) return false;
-
-            // map1[c] = e;
-            // map2[e] = c;
         }
 
         return true;
