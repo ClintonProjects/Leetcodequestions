2796569d8df020989ce0f44e7edfa2a40ebedda2
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 22:30:26 +0100

    Time: 10 ms (77.69%) | Memory: 45.2 MB (80.59%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -9,19 +9,13 @@ public class Solution {
         List<Integer> l1 = new ArrayList<Integer>();
         List<Integer> l2 = new ArrayList<Integer>();
 
-        int N = Math.max(nums1.length, nums2.length);
-
         for (int i : nums1) hs.add(i);
 
-        for (int i : nums2)  hs2.add(i);
+        for (int i : nums2) hs2.add(i);
 
-        for (int i : hs) 
-            if (!hs2.contains(i)) l1.add(i);
-        
+        for (int i : hs) if (!hs2.contains(i)) l1.add(i);
 
-        for (int i : hs2) 
-            if (!hs.contains(i)) l2.add(i);
-        
+        for (int i : hs2) if (!hs.contains(i)) l2.add(i);
 
         List<List<Integer>> n = new ArrayList<List<Integer>>();
         n.add(l1);
