f6298ab4d10e759cd0978bf4da7fb36fddcac2fa
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 16:34:02 +0100

    Time: 1 ms (85.18%) | Memory: 41.2 MB (99.45%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,10 +1,7 @@
 import java.util.*;
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        int counter = 0;
         List<List<Integer>> l = new ArrayList<List<Integer>>();
-        // int n = 1;
-        int length = 0;
 
         for (int i = 0; i < numRows; i++) {
             List<Integer> n = new ArrayList<Integer>();
