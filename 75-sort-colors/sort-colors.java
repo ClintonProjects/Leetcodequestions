172c85d719f630b800b7c7d5da0559be49a4cbbd
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-04-21 21:57:47 +0100

    Time: 0 ms (100.00%) | Memory: 42 MB (19.34%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,15 +1,17 @@
 class Solution {
     public void sortColors(int[] nums) {
+        Arrays.sort(nums);
         
-        int temp;
-        for (int i = 0; i < nums.length; i++) {
-        for (int j = 0; j < nums.length; j++) {
-            temp = nums[i];
-            if (nums[i] < nums[j]) {
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
-        }
-        }
+        //bubble sort first, I knew this be too slow, just did to make sure I understood the question.
+        // int temp;
+        // for (int i = 0; i < nums.length; i++) {
+        // for (int j = 0; j < nums.length; j++) {
+        //     temp = nums[i];
+        //     if (nums[i] < nums[j]) {
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //     }
+        // }
+        // }
     }
 }
