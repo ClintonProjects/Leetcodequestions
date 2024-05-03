d40132059a990a03e810b99d32a59e5a9463e11c
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-03 07:47:19 +0100

    Time: 1 ms (25.00%) | Memory: 43.8 MB (75.42%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -1,14 +1,13 @@
 import java.math.BigInteger;
 
 class Solution {
+
     public int arraySign(int[] nums) {
-        int cur = 1;
+        int cur = 1; // this of number 1 not multiplying the numbers.
         for (int i : nums) {
-            if (i == 0) 
-                return 0;
-    
-            if (i < 0)
-                cur = -cur;
+        if (i == 0) cur *= 0; 
+        else if (i > 0) cur *= 1; 
+        else if (i < 0) cur *= -1; 
         }
 
         return cur;
