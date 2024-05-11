d9733d23859412a1d1b445bf4b278766e6a5dab7
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-11 19:39:27 +0100

    Time: 2 ms (16.37%) | Memory: 41.7 MB (6.69%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,16 +1,11 @@
 class Solution {
-    public int lastStoneWeight(int[] stones) {
-        //improving space complex
-        
-        PriorityQueue<Integer> heap = new PriorityQueue<>(
-             (a, b) -> Integer.compare(b, a)
-        );
 
-        for (int i : stones)  heap.offer(i);
-        while(heap.size() > 1) heap.offer( heap.poll() - heap.poll());
-        
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        for (int i : stones) heap.offer(i);
+        while (heap.size() > 1) heap.offer(heap.poll() - heap.poll());
 
         return heap.poll();
-
     }
 }
