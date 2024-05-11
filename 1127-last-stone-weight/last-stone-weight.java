a12e33568df376dfbe514e2719d0cd31283268c5
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-11 19:38:59 +0100

    Time: 1 ms (98.39%) | Memory: 40.8 MB (78.77%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,17 +1,14 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        //think of bashing stones,
-        //so if we have stone of 4 and smash it vs 2 and then we have 2
-        //if we have stone 5 and we bash vs 4 then we have 1
+        //improving space complex
+        
         PriorityQueue<Integer> heap = new PriorityQueue<>(
              (a, b) -> Integer.compare(b, a)
         );
-        for (int i : stones)  heap.offer(i);
 
-        while(heap.size() > 1) {
-            int t = heap.poll() - heap.poll();
-            heap.offer(t);
-        }
+        for (int i : stones)  heap.offer(i);
+        while(heap.size() > 1) heap.offer( heap.poll() - heap.poll());
+        
 
         return heap.poll();
 
