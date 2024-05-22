ae810f59770632f0a190d17c8d05fcf86a650aaf
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-23 00:17:02 +0100

    Time: 34 ms (47.13%) | Memory: 51.4 MB (66.76%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -2,18 +2,26 @@ class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
         int sum = 0;
-        Set<List<Integer>> distinctList = new HashSet<>();
         List<List<Integer>> setter = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
 
+        int middle = 0;
+        int right = 0;
+
         for (int left = 0; left < nums.length - 2; left++) {
-            int middle = left + 1;
-            int right = nums.length - 1;
+             if (left > 0 && nums[left] == nums[left - 1]) {
+                continue;
+            }
+
+            middle = left + 1;
+            right = nums.length - 1;
 
             while (middle < right) {
                 sum = nums[left] + nums[middle] + nums[right];
                 if (sum == 0) {
-                    distinctList.add(new ArrayList<Integer>(Arrays.asList(nums[left], nums[middle], nums[right])));
+                    setter.add(new ArrayList<Integer>(Arrays.asList(nums[left], nums[middle], nums[right])));
+                    middle = middle(nums, middle, right);
+                    right = right(nums, right, middle);
                     middle++;
                     right--;
                 } else if (sum < 0) {
@@ -24,8 +32,20 @@ class Solution {
             }
         }
 
-        setter.addAll(distinctList);
-
         return setter;
+    }
+
+    public int middle(int[] nums, int currentIndex, int max) {
+        while (currentIndex < max && nums[currentIndex] == nums[currentIndex + 1]) {
+            currentIndex++;
+        }
+        return currentIndex;
+    }
+
+    public int right(int[] nums, int currentIndex, int lowerbound) {
+        while (lowerbound < currentIndex && nums[currentIndex] == nums[currentIndex - 1]) {
+            currentIndex--;
+        }
+        return currentIndex;
     }
 }
