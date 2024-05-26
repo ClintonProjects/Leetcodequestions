abc4044621cbab0c97d27b4fe5ffc158b2e3681d
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-26 04:09:26 +0100

    Time: 2 ms (47.29%) | Memory: 41 MB (18.86%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -4,10 +4,9 @@ class Solution {
      String s;
 
         while (num > 9) {
-         s = String.valueOf(num);
          total = 0;
 
-        for (char c : s.toCharArray()) {
+        for (char c : String.valueOf(num).toCharArray()) {
             total += c - '0';
         }
 
