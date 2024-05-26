06392f5e58289c336272526d8fc1ae58926ac07d
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-26 16:20:32 +0100

    Time: 74 ms (6.08%) | Memory: 44.6 MB (12.22%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,10 +1,41 @@
 import java.math.BigInteger;
 
 class Solution {
+
     public String addStrings(String num1, String num2) {
-        // I'll do this the way it's meant to be done after:
-        BigInteger n1 = new BigInteger(num1);
-        BigInteger n2 = new BigInteger(num2);
-        return n1.add(n2).toString();
+        int l = num1.length() - 1, r = num2.length() - 1;
+        String result = "";
+        int current = 0;
+        boolean carry = false;
+
+        while (l >= 0 || r >= 0 || carry) {
+            if (l >= 0) {
+                 System.out.println("left " + (num1.charAt(l) - '0'));
+                current += num1.charAt(l) - '0';
+                l--;
+            }
+
+            if (r >= 0) {
+                System.out.println("right " + (num2.charAt(r) - '0'));
+                current += num2.charAt(r) - '0';
+                r--;
+            }
+
+
+            if (carry) {
+                current = current + 1;
+                carry = false;
+            }
+
+            if (current >= 10) {
+                current -= 10;
+                carry = true;
+            }
+
+            result = String.valueOf(current) + result;
+            current = 0;
+        }
+
+        return result;
     }
 }
