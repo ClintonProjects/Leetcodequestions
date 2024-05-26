6a4f8f2adcada1785a1fe747de804b31e74118cf
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-26 03:44:30 +0100

    Time: 2 ms (72.85%) | Memory: 41.5 MB (81.74%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,29 +2,14 @@ class Solution {
 
     public char findTheDifference(String s, String t) {
         //I this method be slow, perhaps, hashmap would be faster.
-
-        char[] tArray = s.toCharArray();
-        char[] sArray = t.toCharArray();
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < tArray.length; i++) {
-            map.put(tArray[i], map.getOrDefault(tArray[i], 0) + 1);
-            if (i < sArray.length) {
-                map.put(sArray[i], map.getOrDefault(sArray[i], 0) - 1);
-            }
+        int result = 0;
+        
+         for (int i = 0; i < t.length(); i++) {
+            if (s.length() > i)
+            result += s.charAt(i);
+            result -= t.charAt(i);
         }
 
-
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            char character = entry.getKey();
-            int count = entry.getValue();
-            if (count <= -1) {
-                return character;
-            }
-        }
-
-
-        return sArray[sArray.length - 1];
+        return (char) -result;
     }
 }
