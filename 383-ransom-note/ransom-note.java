2306ae98ad03e5deedceceebec40775d1c9174c3
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-05-26 03:13:38 +0100

    Time: 7 ms (60.07%) | Memory: 44.5 MB (82.12%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,24 +1,17 @@
 class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
         int[] ranArray = new int[255];
-        
+
         for (int i = 0; i < Math.max(ransomNote.length(), magazine.length()); i++) {
-            if (i < ransomNote.length())
-            ranArray[ransomNote.charAt(i) - 'a']++;
-            if (i < magazine.length())
-            ranArray[magazine.charAt(i) - 'a']--;
+            if (i < ransomNote.length()) ranArray[ransomNote.charAt(i) - 'a']++;
+            if (i < magazine.length()) ranArray[magazine.charAt(i) - 'a']--;
         }
 
         Arrays.sort(ranArray);
-
-        if (ranArray[254] > 0)
-        return false;
-
-        // for (int i : ranArray) {
-        //     if (i > 0)
-        //     return false;
-        // }
+        if (ranArray[254] > 0) return false;
 
         return true;
     }
