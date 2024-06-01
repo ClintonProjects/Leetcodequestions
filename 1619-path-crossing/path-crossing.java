d5c01f9fa87860ff4669bce083b75156d1c5909d
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-06-01 17:53:10 +0100

    Time: 1 ms (98.97%) | Memory: 41.9 MB (54.17%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -3,18 +3,15 @@ class Solution {
     public boolean isPathCrossing(String path) {
         int x = 0;
         int y = 0;
-        String t = x + "," + y;
-        HashSet<String> hs = new HashSet<String>();
-        hs.add(t);
+        Set<Pair<Integer,Integer>> hs = new HashSet<>();
+        hs.add(new Pair(0,0));
 
         for (int i = 0; i < path.length(); i++) {
             if (path.charAt(i) == 'N') y++;
             else if (path.charAt(i) == 'S') y--; 
             else if (path.charAt(i) == 'E') x++; 
             else if (path.charAt(i) == 'W') x--;
-            
-            t = x + "," + y;
-            if (!hs.add(t)) return true;
+            if (!hs.add(new Pair(x,y))) return true;
         }
 
         return false;
