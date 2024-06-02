d7839173ff4bccc2237fef874cc97c2470a07e84
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-06-02 17:32:31 +0100

    Time: 3 ms (11.15%) | Memory: 41.1 MB (82.19%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -3,7 +3,6 @@ class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         int arrayIndexL = 0, arrayIndexR = 0;
         int i = 0, j = 0;
-        char c, c2;
 
         while (arrayIndexL < word1.length) {
             if (i == word1[arrayIndexL].length()) {
@@ -17,11 +16,7 @@ class Solution {
             }
 
             if (arrayIndexL == word1.length || arrayIndexR == word2.length) break;
-
-            c = word1[arrayIndexL].charAt(i);
-            c2 = word2[arrayIndexR].charAt(j);
-
-            if (c != c2) return false;
+            if (word1[arrayIndexL].charAt(i) !=  word2[arrayIndexR].charAt(j)) return false;
 
             i++;
             j++;
