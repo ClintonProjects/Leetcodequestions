f7776a3a0b3f34468de042bea7e8282d26933375
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-06-02 17:42:29 +0100

    Time: 0 ms (100.00%) | Memory: 41.4 MB (58.56%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,26 +1,18 @@
 class Solution {
 
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        int arrayIndexL = 0, arrayIndexR = 0, i = 0, j = 0;
+        //this will be slow just making sure I understand the question
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
 
-        while (arrayIndexL < word1.length && arrayIndexR < word2.length) {
-            if (word1[arrayIndexL].charAt(i) != word2[arrayIndexR].charAt(j)) return false;
-
-            if (i == word1[arrayIndexL].length() - 1) {
-                arrayIndexL++;
-                i = 0;
-            } else {
-                i++;
-            }
-
-            if (j == word2[arrayIndexR].length() - 1) {
-                arrayIndexR++;
-                j = 0;
-            } else {
-                j++;
-            }
+        for (String s3 : word1) {
+            sb.append(s3);
         }
 
-        return arrayIndexL == word1.length && arrayIndexR == word2.length;
+        for (String s3 : word2) {
+            sb2.append(s3);
+        }
+
+        return sb.toString().equals(sb2.toString());
     }
 }
