96fb8a90314bd5396c4551415c1b746b70f6378f
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-07-08 13:38:16 +0100

    Time: 28 ms (17.09%) | Memory: 45.5 MB (8.22%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.java
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -1,13 +1,17 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
         Set<String> hs = new HashSet<>();
-        String e;
-        for (String s : emails) {
-            e = s.split("@")[0];
-            e = e.split("\\+")[0];
-            e = e.replaceAll("\\.", "");
-            hs.add(e + "@" + s.split("@")[1]);
+
+        for (String email : emails) {
+            String[] parts = email.split("@");
+            parts[0] = parts[0].replaceAll("\\.", "");
+            if (parts[0].contains("+")) {
+            parts[0] = parts[0].split("\\+")[0];
+            }
+            
+            hs.add(parts[0] + "@" + parts[1]);
         }
+
         return hs.size();
     }
 }
