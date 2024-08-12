c3b394abad35f19b6ec53a487336127304262f83
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2024-08-12 23:33:49 +0100

    Time: 0 ms (100.00%) | Memory: 42 MB (30.04%) - LeetSync

--- 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
@@ -3,16 +3,17 @@ class Solution {
     public int[] numberOfPairs(int[] nums) {
         int[] ls = new int[101];
 
-        for (var i : nums)
+        for (int i : nums)
             ls[i]++;
 
-        int[] r = new int[2];
+        int distinct = 0;
+        int pairs = 0;
 
-        for (var i : ls) {
-            r[0] += i / 2;
-            r[1] += i % 2;
+        for (int i : ls) {
+            pairs += i / 2;
+            distinct += i % 2;
         }
 
-        return r;
+        return new int[] { pairs, distinct };
     }
 }
